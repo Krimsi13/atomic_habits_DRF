bbdb99d740c0a55cd180b8dc22936440f9fb8dca
Krimsi13 <aleksejkrimsi@gmail.com>
2024-07-04 19:44:00 +0300

    create_app_users

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
