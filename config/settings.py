c3253201b69c8b2fb810c08a808eda9d5f8a129e
Krimsi13 <aleksejkrimsi@gmail.com>
2024-07-24 22:06:33 +0300

    coursework_8_docker

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
